754522e0105b05f7a3b9fcfade08478ff6284452
Mauricio Amaral Gonçalves <mauricioamaralg@gmail.com>
2019-03-14 17:01:06 -0300

    SONAr Modules Refactor

--- sonar-node/run.sh
+++ sonar-node/run.sh
@@ -1,63 +1,94 @@
 #!/bin/bash
  
 case ${1,,} in
-	nem)  target="network-event-manager" ;;
-	nsb)  target="network-service-bus" ;;
-	asce) target="alarms-self-collector-entity" ;;
-	msce) target="metrics-self-collector-entity" ;;
-	ssce) target="samples-self-collector-entity" ;;
-	msce) target="topology-self-collector-entity" ;;
-	asle) target="analysis-self-learning-entity" ;;
-	csle) target="configuration-self-learning-entity" ;;
-	psle) target="prediction-self-learning-entity" ;;
-	sce)  target="self-configuration-entity" ;;
-	she)  target="self-healing-entity" ;;
-	sme)  target="self-management-entity" ;;
-	soe)  target="self-optimization-entity" ;;
-	spe)  target="self-planning-entity" ;;
-	sse)  target="self-protection-entity" ;;
-	sd)   target="sonar-dashboard" ;;
+	ascoe) 	target="alarms-self-collector-entity" ;;
+	mscoe) 	target="metrics-self-collector-entity" ;;
+	sscoe) 	target="samples-self-collector-entity" ;;
+	mscoe) 	target="topology-self-collector-entity" ;;
+	lscoe) 	target="logs-self-collector-entity" ;;
+	dsle) 	target="diagnosis-self-learning-entity" ;;
+	rsle) 	target="rating-self-learning-entity" ;;
+	isle) 	target="intent-self-learning-entity" ;;
+	tsle) 	target="tunning-self-learning-entity" ;;
+	psle) 	target="prediction-self-learning-entity" ;;
+	sce)  	target="self-configuration-entity" ;;
+	she)  	target="self-healing-entity" ;;
+	sme)  	target="self-management-entity" ;;
+	soe)  	target="self-optimization-entity" ;;
+	sple)  	target="self-planning-entity" ;;
+	spe)  	target="self-protection-entity" ;;
+	asb)  	target="administration-service-bus" ;;
+	csb)  	target="customer-service-bus" ;;
+	isb)  	target="integration-service-bus" ;;
+	nem)  	target="network-event-manager" ;;
+	abm)  	target="auto-boot-manager" ;;
+	cpi)  	target="control-plane-interceptor" ;;
+	sd)   	target="sonar-dashboard" ;;
 
-	network-event-manager)              target="network-event-manager" ;;
-	network-service-bus)                target="network-service-bus" ;;
-	alarms-self-collector-entity)       target="alarms-self-collector-entity" ;;
-	metrics-self-collector-entity)      target="metrics-self-collector-entity" ;;
-	samples-self-collector-entity)      target="samples-self-collector-entity" ;;
-	topology-self-collector-entity)     target="topology-self-collector-entity" ;;
-	analysis-self-learning-entity)      target="analysis-self-learning-entity" ;;
-	configuration-self-learning-entity) target="configuration-self-learning-entity" ;;
-	prediction-self-learning-entity)    target="prediction-self-learning-entity" ;;
-	self-configuration-entity)          target="self-configuration-entity" ;;
-	self-healing-entity)                target="self-healing-entity" ;;
-	self-management-entity)             target="self-management-entity" ;;
-	self-optimization-entity)           target="self-optimization-entity" ;;
-	self-planning-entity)               target="self-planning-entity" ;;
-	self-protection-entity)             target="self-protection-entity" ;;
-	sonar-dashboard)                    target="sonar-dashboard" ;;
+	alarms-self-collector-entity) 		target="alarms-self-collector-entity" ;;
+	metrics-self-collector-entity) 		target="metrics-self-collector-entity" ;;
+	samples-self-collector-entity) 		target="samples-self-collector-entity" ;;
+	topology-self-collector-entity) 	target="topology-self-collector-entity" ;;
+	logs-self-collector-entity) 		target="logs-self-collector-entity" ;;
+	diagnosis-self-learning-entity) 	target="diagnosis-self-learning-entity" ;;
+	rating-self-learning-entity) 		target="rating-self-learning-entity" ;;
+	intent-self-learning-entity) 		target="intent-self-learning-entity" ;;
+	tunning-self-learning-entity) 		target="tunning-self-learning-entity" ;;
+	prediction-self-learning-entity) 	target="prediction-self-learning-entity" ;;
+	self-configuration-entity)  		target="self-configuration-entity" ;;
+	self-healing-entity)  				target="self-healing-entity" ;;
+	self-management-entity)  			target="self-management-entity" ;;
+	self-optimization-entity)  			target="self-optimization-entity" ;;
+	self-planning-entity)  				target="self-planning-entity" ;;
+	self-protection-entity)  			target="self-protection-entity" ;;
+	administration-service-bus)  		target="administration-service-bus" ;;
+	customer-service-bus)  				target="customer-service-bus" ;;
+	integration-service-bus)  			target="integration-service-bus" ;;
+	network-event-manager)  			target="network-event-manager" ;;
+	auto-boot-manager)  				target="auto-boot-manager" ;;
+	control-plane-interceptor)  		target="control-plane-interceptor" ;;
+	sonar-dashboard)   					target="sonar-dashboard" ;;
 
-	event-manager)          target="network-event-manager" ;;
-	service-bus)            target="network-service-bus" ;;
-	alarms-collector)       target="alarms-self-collector-entity" ;;
-	metrics-collector)      target="metrics-self-collector-entity" ;;
-	samples-collector)      target="samples-self-collector-entity" ;;
-	topology-collector)     target="topology-self-collector-entity" ;;
-	analysis-learning)      target="analysis-self-learning-entity" ;;
-	configuration-learning) target="configuration-self-learning-entity" ;;
-	prediction-learning)    target="prediction-self-learning-entity" ;;
-	configuration-entity)   target="self-configuration-entity" ;;
-	healing-entity)         target="self-healing-entity" ;;
-	management-entity)      target="self-management-entity" ;;
-	optimization-entity)    target="self-optimization-entity" ;;
-	planning-entity)        target="self-planning-entity" ;;
-	protection-entity)      target="self-protection-entity" ;;
-	dashboard)              target="sonar-dashboard" ;;
+	alarms-collector) 		target="alarms-self-collector-entity" ;;
+	metrics-collector) 		target="metrics-self-collector-entity" ;;
+	samples-collector) 		target="samples-self-collector-entity" ;;
+	topology-collector) 	target="topology-self-collector-entity" ;;
+	logs-collector) 		target="logs-self-collector-entity" ;;
+	diagnosis-learning) 	target="diagnosis-self-learning-entity" ;;
+	rating-learning) 		target="rating-self-learning-entity" ;;
+	intent-learning) 		target="intent-self-learning-entity" ;;
+	tunning-learning) 		target="tunning-self-learning-entity" ;;
+	prediction-learning) 	target="prediction-self-learning-entity" ;;
+	configuration-entity)  	target="self-configuration-entity" ;;
+	healing-entity)  		target="self-healing-entity" ;;
+	management-entity)  	target="self-management-entity" ;;
+	optimization-entity)  	target="self-optimization-entity" ;;
+	planning-entity)  		target="self-planning-entity" ;;
+	protection-entity)  	target="self-protection-entity" ;;
+	administration-bus)  	target="administration-service-bus" ;;
+	customer-bus)  			target="customer-service-bus" ;;
+	integration-bus)  		target="integration-service-bus" ;;
+	event-manager)  		target="network-event-manager" ;;
+	boot-manager)  			target="auto-boot-manager" ;;
+	interceptor)  			target="control-plane-interceptor" ;;
+	dashboard)   			target="sonar-dashboard" ;;
 
-	configuration) target="self-configuration-entity" ;;
-	healing)       target="self-healing-entity" ;;
-	management)    target="self-management-entity" ;;
-	optimization)  target="self-optimization-entity" ;;
-	planning)      target="self-planning-entity" ;;
-	protection)    target="self-protection-entity" ;;
+	alarms) 		target="alarms-self-collector-entity" ;;
+	metrics) 		target="metrics-self-collector-entity" ;;
+	samples) 		target="samples-self-collector-entity" ;;
+	topology) 		target="topology-self-collector-entity" ;;
+	logs) 			target="logs-self-collector-entity" ;;
+	diagnosis) 		target="diagnosis-self-learning-entity" ;;
+	rating) 		target="rating-self-learning-entity" ;;
+	intent) 		target="intent-self-learning-entity" ;;
+	tunning) 		target="tunning-self-learning-entity" ;;
+	prediction) 	target="prediction-self-learning-entity" ;;
+	configuration) 	target="self-configuration-entity" ;;
+	healing)       	target="self-healing-entity" ;;
+	management)    	target="self-management-entity" ;;
+	optimization)  	target="self-optimization-entity" ;;
+	planning)      	target="self-planning-entity" ;;
+	protection)    	target="self-protection-entity" ;;
 
 	h) ;;
 	help) ;;
@@ -68,22 +99,29 @@ esac
 if [ 	-z "$target" ]
   then
      echo -e "Usage:"
-     echo -e "\t./run.sh nem|event-manager|network-event-manager -------------------------- runs 'Network-Event-Manager' application as a Docker container."
-     echo -e "\t./run.sh nsb|service-bus|network-service-bus ------------------------------ runs 'Network-Service-Manager' application as a Docker container."
-     echo -e "\t./run.sh asce|alarms-collector|alarms-self-collector-entity --------------- runs 'Alarms Self-Collector Entity' application as a Docker container."
-     echo -e "\t./run.sh msce|metrics-collector|metrics-self-collector-entity ------------- runs 'Metrics Self-Collector Entity' application as a Docker container."
-     echo -e "\t./run.sh ssce|samples-collector|samples-self-collector-entity ------------- runs 'Samples Self-Collector Entity' application as a Docker container."
-     echo -e "\t./run.sh tsce|topology-collector|topology-self-collector-entity ----------- runs 'Topology Self-Collector Entity' application as a Docker container."
-     echo -e "\t./run.sh asle|analysis-learning|analysis-self-learning-entity ------------- runs 'Analysis Self-Learning Entity' application as a Docker container."
-     echo -e "\t./run.sh csle|configuration-learning|configuration-self-learning-entity --- runs 'Configuration Self-Learning Entity' application as a Docker container."
-     echo -e "\t./run.sh psle|prediction-learning|prediction-self-learning-entity --------- runs 'Prediction Self-Learning Entity' application as a Docker container."
-     echo -e "\t./run.sh sce|configuration|configuration-entity|self-configuration-entity - runs 'Self-Configuration Entity' application as a Docker container."
-     echo -e "\t./run.sh she|healing|healing-entity|self-healing-entity ------------------- runs 'Self-Healing Entity' application as a Docker container."
-     echo -e "\t./run.sh sme|management|management-entity|self-management-entity ---------- runs 'Self-Management Entity' application as a Docker container."
-     echo -e "\t./run.sh soe|optimization|optimization-entity|self-optimization-entity ---- runs 'Self-Optimization Entity' application as a Docker container."
-     echo -e "\t./run.sh spe|planning|planning-entity|self-planning-entity ---------------- runs 'Self-Planning Entity' application as a Docker container."
-     echo -e "\t./run.sh sse|protection|protection-entity|self-protection-entity ---------- runs 'Self-Protection Entity' application as a Docker container."
-     echo -e "\t./run.sh h|help ----------------------------------------------------------- shows the information above."
+     echo -e "\t./build.sh ascoe|alarms|alarms-collector|alarms-self-collector-entity --------------- runs 'Alarms Self-Collector Entity' application as a Docker Container."
+     echo -e "\t./build.sh mscoe|metrics|metrics-collector|metrics-self-collector-entity ------------ runs 'Metrics Self-Collector Entity' application as a Docker Container."
+     echo -e "\t./build.sh sscoe|samples|samples-collector|samples-self-collector-entity ------------ runs 'Samples Self-Collector Entity' application as a Docker Container."
+     echo -e "\t./build.sh tscoe|toplogy|topology-collector|topology-self-collector-entity ---------- runs 'Topology Self-Collector Entity' application as a Docker Container."
+     echo -e "\t./build.sh lscoe|logs|logs-collector|logs-self-collector-entity --------------------- runs 'Logs Self-Collector Entity' application as a Docker Container."
+     echo -e "\t./build.sh dsle|diagnosis|diagnosis-learning|diagnosis-self-learning-entity --------- runs 'Diagnosis Self-Learning Entity' application as a Docker Container."
+     echo -e "\t./build.sh tsle|tunning|tunning-learning|tunning-self-learning-entity --------------- runs 'Tunning Self-Learning Entity' application as a Docker Container."
+     echo -e "\t./build.sh psle|prediction|prediction-learning|prediction-self-learning-entity ------ runs 'Prediction Self-Learning Entity' application as a Docker Container."
+     echo -e "\t./build.sh rsle|rating|rating-learning|rating-self-learning-entity ------------------ runs 'Rating Self-Learning Entity' application as a Docker Container."
+     echo -e "\t./build.sh isle|intent|intent-learning|intent-self-learning-entity ------------------ runs 'Intent Self-Learning Entity' application as a Docker Container."
+     echo -e "\t./build.sh sce|configuration|configuration-entity|self-configuration-entity --------- runs 'Self-Configuration Entity' application as a Docker Container."
+     echo -e "\t./build.sh she|healing|healing-entity|self-healing-entity --------------------------- runs 'Self-Healing Entity' application as a Docker Container."
+     echo -e "\t./build.sh sme|management|management-entity|self-management-entity ------------------ runs 'Self-Management Entity' application as a Docker Container."
+     echo -e "\t./build.sh soe|optimization|optimization-entity|self-optimization-entity ------------ runs 'Self-Optimization Entity' application as a Docker Container."
+     echo -e "\t./build.sh sple|planning|planning-entity|self-planning-entity ----------------------- runs 'Self-Planning Entity' application as a Docker Container."
+     echo -e "\t./build.sh spe|protection|protection-entity|self-protection-entity ------------------ runs 'Self-Protection Entity' application as a Docker Container."
+     echo -e "\t./build.sh asb|administration-bus|administration-service-bus ------------------------ runs 'Administration-Service-Manager' application as a Docker Container."
+     echo -e "\t./build.sh csb|customer-bus|customer-service-bus ------------------------------------ runs 'Customer-Service-Manager' application as a Docker Container."
+     echo -e "\t./build.sh isb|integration-bus|integration-service-bus ------------------------------ runs 'Integration-Service-Manager' application as a Docker Container."
+     echo -e "\t./build.sh nem|event-manager|network-event-manager ---------------------------------- runs 'Network-Event-Manager' application as a Docker Container."
+     echo -e "\t./build.sh abm|boot-manager|auto-boot-manager --------------------------------------- runs 'Auto-Boot-Manager' application as a Docker Container."
+     echo -e "\t./build.sh cpi|interceptor|control-plane-interceptor -------------------------------- runs 'Control-Plane-Interceptor' application as a Docker Container."
+     echo -e "\t./build.sh h|help ------------------------------------------------------------------- shows the information above."
 else
      docker run -i -t --rm meharsonar/$target
 fi
