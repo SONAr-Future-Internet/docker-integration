1b5a8ba0f06a45bb2100fffe21b27911e122503b
Mauricio Amaral Gonçalves <mauricioamaralg@gmail.com>
2019-03-22 16:36:04 -0300

    Improvement in openvswitch image - update of openvswitch, alpine, dhcp, snmpd and lldpd

---
+++ openvswitch/v2/scripts/boot.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+if [ ! -d "/var/run/openvswitch" ]
+then
+  mkdir /var/run/openvswitch
+fi
+
+if [ ! -f "/etc/openvswitch/conf.db" ]
+then
+  ovsdb-tool create /etc/openvswitch/conf.db /usr/share/openvswitch/vswitch.ovsschema
+
+  ovsdb-server --detach --remote=punix:/var/run/openvswitch/db.sock
+  ovs-vswitchd --detach --pidfile=/var/run/openvswitch/ovs-vswitchd.pid 
+  ovs-vsctl --no-wait init
+
+  ovs-vsctl add-br br0
+  #ovs-vsctl set bridge br0 stp_enable=true
+
+  x=0
+  until [ $x = "16" ]; do
+    ovs-vsctl add-port br0 eth$x
+    ovs-vsctl set Interface eth$x lldp:enable=true
+    x=$((x+1))
+  done
+
+else
+  ovsdb-server --detach --remote=punix:/var/run/openvswitch/db.sock
+  ovs-vswitchd --detach --pidfile=/var/run/openvswitch/ovs-vswitchd.pid
+fi
+
+ip link set dev br0 up
+
+dhclient br0
+
+/bin/bash
