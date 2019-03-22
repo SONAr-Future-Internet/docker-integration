99f70359695753cf3c102bd644674df0ebea809e
Mauricio Amaral Gonçalves <mauricioamaralg@gmail.com>
2019-03-22 09:57:29 -0400

    Build fix of openvswitch : Proxy Support

--- openvswitch/build.sh
+++ openvswitch/build.sh
@@ -1,1 +1,5 @@
-docker build -f Dockerfile -t meharsonar/openvswitch .
+#Build without proxy
+#docker build -f Dockerfile -t meharsonar/openvswitch .
+
+#Build with proxy
+docker build -f Dockerfile -t meharsonar/openvswitch --build-arg http_proxy=http://200.131.199.26:3128 --build-arg https_proxy=http://200.131.199.26:3128 .
