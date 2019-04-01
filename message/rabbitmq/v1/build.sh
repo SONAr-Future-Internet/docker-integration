bd9530c52427be7caa67c25b65f68994162459ef
Mauricio Amaral Gonçalves <mauricioamaralg@gmail.com>
2019-04-01 16:15:57 -0300

    Creation of new image for message-queue

---
+++ message/rabbitmq/v1/build.sh
@@ -0,0 +1,5 @@
+#Build without proxy
+#docker build -f Dockerfile -t meharsonar/rabbitmq .
+
+#Build with proxy
+docker build -f Dockerfile -t meharsonar/rabbitmq --build-arg http_proxy=http://200.131.199.26:3128 --build-arg https_proxy=http://200.131.199.26:3128 .
---
+++ message/rabbitmq/v1/run.sh
@@ -0,0 +1,1 @@
+docker run -i -t --rm -p 127.0.0.1:9042:9042 -p 127.0.0.1:9160:9160 meharsonar/rabbitmq
